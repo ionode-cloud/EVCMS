68fce4ee6d23229fa30135bd245bd35444d3379f
Yash Vardhan <vardhan.yash3105@gmail.com>
2025-11-13 17:03:10 +0530

    chore(stack): change code

--- EV-backend/server.js
+++ EV-backend/server.js
@@ -5,8 +5,13 @@ import crypto from "crypto";
 import Razorpay from "razorpay";
 import { log } from "console";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -46,7 +51,7 @@ const User = mongoose.model("User", userProfile);
 //  Allow your React app origin
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend port
+    origin: ["http://localhost:5173", "http://evcms.ionoide.cloud"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -258,6 +263,10 @@ app.delete("/stations/:id", async (req, res) => {
 });
 
 // ---------------- Start Server ----------------
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+});
 const PORT = process.env.PORT || 38923;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
